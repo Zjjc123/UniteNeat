b31f6b9b597f9b077af5b024452b6df3d17ea02f
Jason Zhang <zjjc123@hotmail.com>
2020-03-13 15:22:53 -0700

    Nodes and Connections

---
+++ UniteNeat/Assets/NEAT/Structural/Connection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Connections
+{
+    // Connection Properties
+    private int _in;
+    private int _out;
+    private float _weight;
+    private bool _expressed;
+    private int _innovation;
+
+    // Constructor
+    public Connections(int inNode, int outNode, float weight, bool expressed, int innovation)
+    {
+        _in = inNode;
+        _out = outNode;
+        _weight = weight;
+        _expressed = expressed;
+        _innovation = innovation;
+    }
+
+    // Getters and Setters
+    public int InNode
+    {
+        get { return _in; }
+    }
+
+    public int OutNode
+    {
+        get { return _out; }
+    }
+
+    public float Weight
+    {
+        get { return _weight; }
+        set { _weight = value; }
+    }
+
+    public bool Expressed
+    {
+        get { return _expressed; }
+        set { _expressed = value; }
+    }
+
+    public void Disable()
+    {
+        this.Expressed = false;
+    }
+
+    public int Innovation
+    {
+        get { return _innovation; }
+    }
+
+}
---
+++ UniteNeat/Assets/NEAT/Structural/Node.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Node
+{
+    // Types of Nodes
+    public enum NodeType
+    {
+        INPUT,
+        HIDDEN,
+        OUTPUT
+    };
+
+    // Node Properties
+    private NodeType _type;
+    private int _id;
+    private float _value = 0f;
+    
+    // Constructor
+    public Node(NodeType type, int id)
+    {
+        _type = type;
+        _id = id;
+    }
+
+    // Copy Constructor
+    public Node(Node n)
+    {
+        _type = n.Type;
+        _id = n.Id;
+        _value = n.Value;
+    }
+
+    // Getters and Setters
+    public NodeType Type
+    {
+        get { return _type; }
+    }
+    public float Value
+    {
+        get { return _value; }
+        set { _value = value; }
+    }
+
+    public int Id
+    {
+        get { return _id; }
+    }
+
+
+}
