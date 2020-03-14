c2a06c3ce46f9a24174f2944096a86d204f57e2f
Jason Zhang <zjjc123@hotmail.com>
2020-03-14 15:59:31 -0700

    Update History Class

--- UniteNeat/Assets/NEAT/History.cs
+++ UniteNeat/Assets/NEAT/History.cs
@@ -3,18 +3,44 @@ using System.Collections.Generic;
 
 public class History
 {
-    public static List<Connection> GenerationalHistory;
-    private int innovation = 0;
+    private static List<Connection> GenerationalHistory;
+    private static int innovation = 0;
 
-    public int GetInnovation()
+    
+    private static int IncrementAndGetInnovation()
     {
         innovation++;
         return innovation;
     }
 
-    public void ClearHistory()
+    private static int GetCurrentInnovation()
+    {
+        return innovation;
+    }
+  
+
+    public static void ClearHistory()
     {
         GenerationalHistory.Clear();
+    }
+
+    // Check if contains history if it does use historical innovation, if not then increment use new innovation
+    public static void AlterInnovationBasedOnHistory(Connection con)
+    {
+        foreach (Connection c in GenerationalHistory)
+        {
+            if (c.InNode == con.InNode && c.OutNode == con.OutNode)
+            {
+                con.Innovation = c.Innovation;
+                return;
+            }
+        }
+
+        con.Innovation = IncrementAndGetInnovation();
+        Connection newC = new Connection(con.InNode, con.OutNode, 0.0f, false, con.Innovation);
+        
+        GenerationalHistory.Add(newC);
+        return;
     }
 
 }
