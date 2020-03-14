cfdb2f4b9ed14bedd9f1ed1e57d5f9a96df1f89f
Jason Zhang <zjjc123@hotmail.com>
2020-03-14 16:16:42 -0700

    Connection Mutation

--- UniteNeat/Assets/NEAT/Structural/Genome.cs
+++ UniteNeat/Assets/NEAT/Structural/Genome.cs
@@ -79,31 +79,137 @@ public class Genome
     {
         var rand = new Random();
 
-        foreach (KeyValuePair<int, Connection> c in Connections)
+        foreach (Connection c in Connections.Values)
         {
             double r = rand.NextDouble();
             // If perturb
             if (r < PROBABILITY_PERTURBING)
             {
                 // Add plus or minus PERTURB_MAX
-                c.Value.Weight += (float)rand.NextDouble() * PERTURB_MAX - PERTURB_MAX;
+                c.Weight += (float)rand.NextDouble() * PERTURB_MAX - PERTURB_MAX;
             }
             else
             {
                 // Random weights
-                c.Value.Weight = (float)rand.NextDouble() * 2 - 1;
+                c.Weight = (float)rand.NextDouble() * 2 - 1;
             }
 
             // Limit to -1 and 1
-            c.Value.Weight = Math.Max(c.Value.Weight, -1);
-            c.Value.Weight = Math.Min(c.Value.Weight, 1);
+            c.Weight = Math.Max(c.Weight, -1);
+            c.Weight = Math.Min(c.Weight, 1);
         }
     }
 
     // Connection Mutation
-    private void ConnectionMutation()
+    private bool ConnectionMutation()
     {
+        List<Tuple<int, int>> tried = new List<Tuple<int, int>>();
 
+        var rand = new Random();
+
+        bool found = false;
+
+        while (!found)
+        {
+
+            if (tried.Count >= Nodes.Count * Nodes.Count)
+                return false;
+
+            int nodeInID = rand.Next(1, Nodes.Count);
+            int nodeOutID = rand.Next(1, Nodes.Count);
+
+            Tuple<int, int> t = new Tuple<int, int>(nodeInID, nodeOutID);
+
+            if (!tried.Contains(t))
+            {
+                tried.Add(t);
+            }
+
+            Node nodeIn = Nodes[nodeInID];
+            Node nodeOut = Nodes[nodeOutID];
+
+
+            bool impossible = false;
+
+            // Check if connection already exists
+            foreach (Connection c in Connections.Values)
+            {
+                if ((c.InNode == nodeInID && c.OutNode == nodeOutID)
+                    || (c.InNode == nodeOutID && c.OutNode == nodeInID))
+                {
+                    impossible = true;
+                }
+            }
+
+            // Check if connection is possible
+            if (nodeIn.Type == Node.NodeType.INPUT && nodeOut.Type == Node.NodeType.INPUT)
+            {
+                impossible = true;
+            }
+            else if (nodeIn.Type == Node.NodeType.OUTPUT && nodeOut.Type == Node.NodeType.OUTPUT)
+            {
+                impossible = true;
+            }
+
+            // If it exists or not allowed --> next try
+            if (impossible)
+            {
+                continue;
+            }
+
+
+
+            // Get Correct Orientation
+            bool reversed = false;
+
+            // Cases that should be reversed
+            // Case 1: Hidden --> Input
+            if (nodeIn.Type == Node.NodeType.HIDDEN && nodeOut.Type == Node.NodeType.INPUT)
+            {
+                reversed = true;
+            }
+            // Case 2: Output --> Hidden
+            else if (nodeIn.Type == Node.NodeType.OUTPUT && nodeOut.Type == Node.NodeType.HIDDEN)
+            {
+                reversed = true;
+            }
+            // Case 3: Output --> Input
+            else if (nodeIn.Type == Node.NodeType.OUTPUT && nodeOut.Type == Node.NodeType.INPUT)
+            {
+                reversed = true;
+            }
+
+            // If should be reversed then reverse
+            if (reversed)
+            {
+                nodeIn = Nodes[nodeOutID];
+                nodeOut = Nodes[nodeInID];
+            }
+
+            // Check if a connection creates a loop
+            if (IsLoopCreated(nodeInID, nodeOutID))
+            {
+                continue;
+            }
+
+            // Generate Weight
+            float weight = (float)rand.NextDouble() * 2 - 1;
+
+            // Contains history automatically adds new connections to history if does not exist
+            // Use placeholder innovation
+            Connection newConnection = new Connection(nodeInID, nodeOutID, weight, true, 0);
+
+            // Alter innovation based on history
+            History.AlterInnovationBasedOnHistory(newConnection);
+
+            // Add connection to list of connections
+            Connections.Add(newConnection.Innovation, newConnection);
+
+            found = true;
+
+        }
+
+        return true;
     }
 
     // Node Mutation
@@ -112,4 +218,35 @@ public class Genome
 
     }
 
+
+    // ==================================
+    //              Helpers
+    // ==================================
+
+    // Loop Connection Checker
+    // Use recursion to add output nodes to a list 
+    // If new connection connects from an existing out node to the in node a loop will form
+    private bool IsLoopCreated(int inID, int outID)
+    {
+        List<int> outNodes = new List<int>();
+        AddReachableNodes(outNodes, outID);
+        return outNodes.Contains(inID);
+    }
+
+    // Add all the reachable nodes from an input node to a list
+    private void AddReachableNodes(List<int> OutNodesList, int start)
+    {
+        // Add outnode
+        OutNodesList.Add(start);
+        foreach (Connection connection in connections.Values)
+        {
+            // If any connections c start with the outnode --> add c's outnode 
+            // run recursion until all reachable nodes will be added
+            // No infinite loop because there will not be any loop created in the first place
+            if (connection.InNode == start)
+            {
+                AddReachableNodes(OutNodesList, connection.OutNode);
+            }
+        }
+    }
 }
