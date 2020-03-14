2174ff703e4e3a28e01842877522f9b37cca4ac3
Jason Zhang <zjjc123@hotmail.com>
2020-03-14 15:32:24 -0700

    Innovation History

---
+++ UniteNeat/Assets/NEAT/History.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class History
+{
+    public static List<Connection> GenerationalHistory;
+    private int innovation = 0;
+
+    public int GetInnovation()
+    {
+        innovation++;
+        return innovation;
+    }
+
+    public void ClearHistory()
+    {
+        GenerationalHistory.Clear();
+    }
+
+}
